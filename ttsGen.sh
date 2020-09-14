14da233b0e1c0b6c752fcc57759de68ac56faf7d
asin-edu <asin473@aucklanduni.ac.nz>
2020-09-14 12:23:06 +1200

    Finished and tested file, redirected ffmpeg and play output to /dev/null

--- ttsGen.sh
+++ ttsGen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo $0 | text2wave -o
-ffmpeg -i ./Ensoniq-ZR-76-01-Dope-77.wav -filter:a "atempo=0.5" 0.5song.wav
+echo $1 | text2wave -o spokenText.wav
+ffmpeg -y -i spokenText.wav -filter:a "atempo=$2" spokenTextAdj.wav > /dev/null 2>&1
+play -q spokenTextAdj.wav > /dev/null 2>&1
 
