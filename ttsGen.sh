fe1d03a383a2b4d109dd7ef92ef0a9c80601fcf7
asin-edu <asin473@aucklanduni.ac.nz>
2020-09-14 12:28:05 +1200

    Added the removal of audio files once created for a cleaner working directory

--- ttsGen.sh
+++ ttsGen.sh
@@ -3,4 +3,6 @@
 echo $1 | text2wave -o spokenText.wav
 ffmpeg -y -i spokenText.wav -filter:a "atempo=$2" spokenTextAdj.wav > /dev/null 2>&1
 play -q spokenTextAdj.wav > /dev/null 2>&1
+rm spokenText.wav spokenTextAdj.wav
+
 
