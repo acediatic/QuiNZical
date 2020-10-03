187a5f464be8913b5445999eb3fa42c2533027a7
Osama Kashif <osamakashif1@hotmail.com>
2020-10-04 00:23:32 +1300

    Fixed Memory_maker.java updated

--- Assignment_3_and_Project/src/database/Memory_maker.java
+++ Assignment_3_and_Project/src/database/Memory_maker.java
@@ -14,7 +14,10 @@ public class Memory_maker {
 	
 	public static void historyStart(List<Category> categories) throws Exception {
 		//The absolute path is found regardless of location of the code.
-			String path = GameMainController.path;
+		String path = GameMainController.path;
+		//	String fullPath = (new File((new File(System.getProperty("java.class.path"))).getAbsolutePath())).getAbsolutePath();
+		//	String [] relevantPath = fullPath.split(":");
+		//	String path = (new File(relevantPath[0])).getParentFile().getAbsolutePath();
 			
 			// The winnings file is created and stored with 0 initially.
 			File winnings = new File(path + "/.winnings");
@@ -47,26 +50,12 @@ public class Memory_maker {
 					}
 					//Add clue stuff here
 					File inputFile = new File(path+"/.History/"+category.categoryName());
-					//File tempFile = new File(path+"/.History/myTempFile.txt");
-
-					//BufferedReader reader = new BufferedReader(new FileReader(inputFile));
 					BufferedWriter writer = new BufferedWriter(new FileWriter(inputFile));
 					for (Clue clue: category.getAllClues()) {
 						String lineToAdd = clue.showClue() +"@"+clue.showClueType()+"@"+clue.showAnswer()+"@"+clue.showValue()+"@false";
 						writer.write(lineToAdd + System.getProperty("line.separator"));
 					}
-
-					/*String lineToRemove = clue.showValue()+","+clue.showClue()+","+clue.showAnswer();
-					String currentLine;
-					while((currentLine = reader.readLine()) != null) {
-					    if(!currentLine.equals(lineToRemove)) {
-					    	writer.write(currentLine + System.getProperty("line.separator"));
-					    }
-					}*/
 					writer.close(); 
-					/*reader.close();
-					tempFile.renameTo(inputFile);*/
-					
 				}
 			}
 			else if (!history.exists()) {
@@ -75,62 +64,14 @@ public class Memory_maker {
 			
 	}
 	
-	/**
-	 * The historyStart() method is very important.
-	 * It is what is used to initially make the progress files for the history and winnings of the user.
-	 * @throws Exception
-	 */
-	/*public static void historyStart() throws Exception {
-		//The absolute path is found regardless of location of the code.
-		String fullPath = (new File((new File(System.getProperty("java.class.path"))).getAbsolutePath())).getAbsolutePath();
-		String [] relevantPath = fullPath.split(":");
-		String path = (new File(relevantPath[0])).getParentFile().getAbsolutePath();
-		File categoriesDirectory = new File(path+"/.custom_categories");
-		if (!categoriesDirectory.exists()) {
-			throw new Exception("Categories folder not found.");
-		}
-		// It initially makes the directory, and checks if t is made.
-		File history = new File(path + "/.History");
-		Boolean successfullyMade = false;
-		if (!history.exists()) {
-			successfullyMade = history.mkdir();
-		}
-		
-		// If it is, it copies all the files in categories into it.
-		if (successfullyMade) {
-			File [] categoryFiles = categoriesDirectory.listFiles();
-			for (File categoryFile: categoryFiles) {
-				try {
-					Files.copy(Paths.get(path+"/categories/"+categoryFile.getName()), Paths.get(path+"/.History/"+categoryFile.getName()));
-				} catch (IOException e) {
-				e.printStackTrace();
-				}
-			}
-		}
-		else {
-			throw new Exception("History could not be made.");
-		}
-		
-		// The winnings file is created and stored with 0 initially.
-		File winnings = new File(path + "/.winnings");
-		winnings.createNewFile();
-		if (winnings.exists()) {
-			BufferedWriter initialWriter = new BufferedWriter(new FileWriter(winnings));
-			initialWriter.write("0.0");
-			initialWriter.close();
-		}
-		else {
-			throw new Exception("Winnings not made.");
-		}
-		
-	}*/
-	
-	public static void update(Clue clue, String category, Boolean correct) throws Exception {
+	public static void update(Clue clue, Category category, Boolean correct) throws Exception {
 		//Updating in the Model as well.
-		//GameMainController.getModel().markClueAsAnswered(category, clue);
-		clue.answered();
+		Main.getData().markClueAsAnswered(category, clue);
 		//The absolute path is found regardless of location of the code.
 		String path = GameMainController.path;
+		//String fullPath = (new File((new File(System.getProperty("java.class.path"))).getAbsolutePath())).getAbsolutePath();
+		//String [] relevantPath = fullPath.split(":");
+		//String path = (new File(relevantPath[0])).getParentFile().getAbsolutePath();
 		File winnings = new File(path + "/.winnings");
 		// Winnings should be present when updating them, so if it's not, an exception is thrown.
 		if (!winnings.exists()){
@@ -151,21 +92,10 @@ public class Memory_maker {
 		while((winningLine = winningReader.readLine()) != null) {
 			int alreadyWon = Integer.parseInt(winningLine);
 			int result = alreadyWon;
-			/*//Check 1
-			System.out.println(winningLine);
-			System.out.println(alreadyWon);
-			System.out.println(correct);
-			System.out.println(result);
-			//Check 1 done*/
 			if (correct) {
 				result = alreadyWon + clue.returnValue();
-				/*//Check 1
-				System.out.println(clue.showClue());
-				System.out.println(clue.returnValue());
-				System.out.println(correct);
-				System.out.println(result);
-				//Check 1 done*/
 			}
+			//Older code to deduct value on wrong naswer, but not doing that for this.
 			/*else {
 				result = alreadyWon - clue.returnValue();
 			}*/
@@ -183,7 +113,7 @@ public class Memory_maker {
 		
 		// The clue answered has its file located in history, and the file is copied line by line, excluding
 		// the one of the clue answered, and then the new file gets renamed to the old one, replacing it.
-		File inputFile = new File(path+"/.History/"+category);
+		File inputFile = new File(path+"/.History/"+category.categoryName());
 		File tempFile = new File(path+"/.History/myTempFile.txt");
 
 		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
@@ -212,6 +142,9 @@ public class Memory_maker {
 	public static void reset() {
 		//The absolute path is found regardless of location of the code.
 		String path = GameMainController.path;
+		//String fullPath = (new File((new File(System.getProperty("java.class.path"))).getAbsolutePath())).getAbsolutePath();
+		//String [] relevantPath = fullPath.split(":");
+		//String path = (new File(relevantPath[0])).getParentFile().getAbsolutePath();
 		File history = new File(path + "/.History");
 		File winnings = new File(path + "/.winnings");
 		//Checks the files and deletes them if they exist using deleteDir.
