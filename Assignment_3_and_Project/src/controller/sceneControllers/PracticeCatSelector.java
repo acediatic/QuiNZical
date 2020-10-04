8c794f6e03521b9b597f051642d3d55526d87ebc
Adam Sinclair <hello@adamsinclair.dev>
2020-10-05 11:34:39 +1300

    Edited Cosmetics (stroke for winings screen, button size for correct scene) in game

--- Assignment_3_and_Project/src/controller/sceneControllers/PracticeCatSelector.java
+++ Assignment_3_and_Project/src/controller/sceneControllers/PracticeCatSelector.java
@@ -5,6 +5,7 @@ import controller.PrimaryController;
 import database.Category;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
 import service.AskPracticeQuestionService;
 import service.FXMLService;
 
@@ -12,10 +13,23 @@ public class PracticeCatSelector extends Controller {
 	@FXML
 	private ComboBox<Category> combo;
 	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@FXML
 	private void initialize() {
 		combo.getItems().addAll(PracticeModuleController.getInstance().getAllCategories());
 		combo.setEditable(false);
+		combo.setButtonCell(new ListCell(){
+
+	        @Override
+	        protected void updateItem(Object item, boolean empty) {
+	            super.updateItem(item, empty); 
+	            if(empty || item==null){
+	                // styled like -fx-prompt-text-fill:
+	                setStyle("-fx-text-fill: white; -fx-font-size: 1.25em; -fx-font-style: italic");
+	            }
+	        }	
+
+	    });
 		}
 
 	@FXML 
