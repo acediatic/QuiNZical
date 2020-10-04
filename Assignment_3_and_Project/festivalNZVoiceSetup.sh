f31971f0462d3c9859f4ff08704b11b0e85dd75b
Osama Kashif <osamakashif1@hotmail.com>
2020-10-05 00:27:45 +1300

    Found a typo and fixed it

--- Assignment_3_and_Project/festivalNZVoiceSetup.sh
+++ Assignment_3_and_Project/festivalNZVoiceSetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ( ! -d "/usr/share/festival/voices/english/unzip akl_nz_jdt_diphone" ) -a ( -e "$pwd/akl_nz_jdt_diphone.zip" ) ]
+if [ ( ! -d "/usr/share/festival/voices/english/akl_nz_jdt_diphone" ) -a ( -e "$pwd/akl_nz_jdt_diphone.zip" ) ]
 then
 	sudo unzip akl_nz_jdt_diphone.zip -d /usr/share/festival/voices/english
 fi
